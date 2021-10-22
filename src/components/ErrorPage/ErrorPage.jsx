9ae33e75c0266300fc275b9f657ec10c7d3bc6e0
Grzegorz Celek <g.celek@gmail.com>
2021-10-22 13:26:21 +0200

    .

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
@@ -9,6 +10,7 @@ const ErrorPage = () => {
       <p className="">
         Błąd podczas przesyłania danych. Nie powinieneś się znaleźć na tej
         stronie.
+        <Link to="/"> Wróć na stronę główną</Link>
       </p>
     </div>
   );
